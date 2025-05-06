9b9006be407ef05d882a9f6367825a5ad7810790
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-05-06 22:09:30 +0800

    Fix gradio space ssr_mode missing markdown badge

--- gradio/gradio_app.py
+++ gradio/gradio_app.py
@@ -124,4 +124,4 @@ with gr.Blocks(title="Image Faulty Demo") as demo:
     )
 
 if __name__ == '__main__':
-    demo.launch(server_name="0.0.0.0", server_port=7860, mcp_server=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, mcp_server=True, ssr_mode=False)
